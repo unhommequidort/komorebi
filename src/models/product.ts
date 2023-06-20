decb730dd29be423267ea332b831a3eec3a71bfa
Matthew Lyons <mattewjosephlyons@gmail.com>
2023-06-20 17:40:26 -0500

    Create Product model

---
+++ src/models/product.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from 'mongoose';
+
+const productSchema = new Schema<IProduct>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+interface IProduct {
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  createdAt: string;
+  updatedAt: string;
+}
