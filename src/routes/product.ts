e8541b5c14a538c83182b8b01113c397317c03ec
Matthew Lyons <mattewjosephlyons@gmail.com>
2023-06-20 20:27:52 -0500

    Create getProductById route

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,9 +1,14 @@
 import express from 'express';
-import { createProduct, getProducts } from '../controller/product';
+import {
+  createProduct,
+  getProductById,
+  getProducts,
+} from '../controller/product';
 
 const productRoutes = express.Router();
 
 productRoutes.get('/', getProducts);
+productRoutes.get('/:id', getProductById);
 productRoutes.post('/', createProduct);
 
 export default productRoutes;
